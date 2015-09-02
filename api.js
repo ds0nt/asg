48ab60ab3a2bd666af232163cb79c525a6dc1858
Daniel Sont <dan.sont@gmail.com>
2015-09-03 05:17:43 +0700

    pushing

--- api.js
+++ api.js
@@ -1,13 +1,17 @@
-
 import { template, config, engine } from './core'
-import { get, post, listen } from './lib/api'
+import {get,post,listen} from './lib/api'
 
-get `/template` (() => template.list() )
-get `/template/:name`  (name => template.get(name) )
-post `/template/:name` ((name, {data}) => template.save(name, data) )
+get `/template` (template.list)
+get `/template/:name` (
+  name => template.get(name)
+)
+post `/template/:name` (
+  (name, {data}) => template.save(name, data)
+)
+get `/config` (config.list)
 
-get `/config` (() => config.list() )
-get `/config/:name`  (name => config.get(name) )
+get `/config/:name` (name => config.get(name) )
+
 post `/config/:name` ((name, {data}) => config.save(name, data) )
 
 get `/render` ((template, config) => engine.render(template, config) )
